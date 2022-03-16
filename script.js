6e6ffca037566a5c0eb2aff376630cde90f810fa
fizzara <fizzaahmed2000@hotmail.com>
2022-03-16 17:23:37 -0400

    finished adding character design
    also added larger margin for larger screen sizes

--- script.js
+++ script.js
@@ -42,3 +42,35 @@ function showSlides(n, no) {
   x[slideIndex[no]-1].style.display = "block";
   dots[slideIndex[no]-1].className += " active";
 }
+
+
+
+
+
+// Next/previous controls
+function addSlides(n) {
+  displaySlides(charSlideIndex += n);
+}
+
+// Thumbnail image controls
+function shownSlide(n) {
+  displaySlides(charSlideIndex = n);
+}
+
+function displaySlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("char-slide");
+  var dots = document.getElementsByClassName("char-img");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {charSlideIndex = 1}
+  if (n < 1) {charSlideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[charSlideIndex-1].style.display = "block";
+  dots[charSlideIndex-1].className += " active";
+  captionText.innerHTML = dots[charSlideIndex-1].alt;
+}
